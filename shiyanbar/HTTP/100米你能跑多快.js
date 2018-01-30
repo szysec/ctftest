6791aebe61b76bc4fc0c495ad1e1ea4d8f9ba7d3
szysec <szysec@163.com>
2018-01-30 14:14:46 +0800

    add

---
+++ shiyanbar/HTTP/100米你能跑多快.js
@@ -0,0 +1,11 @@
+//100米你能跑多快？ 预备，跑！
+//题目链接，http://ctf5.shiyanbar.com/jia/
+function getTextByName(name) {
+	return document.getElementsByName(name)[0].innerText;
+}
+function fillTextByName(name, text) {
+	var names = document.getElementsByName(name)
+    names[0].value = text;
+}
+res = eval(getTextByName("my_expr").replace(/x/g,"*"));
+fillTextByName("pass_key", res);
